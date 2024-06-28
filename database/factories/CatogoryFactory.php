a4f70436dc9bf4ae0a4516ec2c44ecf565e3ad1a
Bidhun <bidhunvaikkath@gmail.com>
2024-06-28 10:59:19 -0700

    make migrations and seed catoegry table with some data

---
+++ database/factories/CatogoryFactory.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Category;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Catogory>
+ */
+class CatogoryFactory extends Factory
+{
+    protected $model = Category::class;
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        $categories = [
+            'Utilities', 'Rent', 'Transportation', 'Travel', 'Personal Care',
+            'Food', 'Insurance', 'Entertainment', 'Education', 'Debt Payments',
+            'Savings', 'Contingency Expenses'
+        ];
+        return [
+            'category' => $this->faker->unique()->randomElement($categories),
+        ];
+    }
+}
